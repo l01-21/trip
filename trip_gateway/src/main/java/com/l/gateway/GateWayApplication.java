78e59b70b503eea69c7537dbff24d1954e1ffc1d
蓝T <2839942595@qq.com>
2024-07-01 15:37:57 +0800

    [项目初始化] lhy: 新增公共模块（trip_common）、 新增项目网关模块（trip_gateway）、 新增微服务API聚合模块（trip_modules_api）、 新增微服务聚合模块（trip_modules）

---
+++ trip_gateway/src/main/java/com/l/gateway/GateWayApplication.java
@@ -0,0 +1,18 @@
+package com.l.gateway;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * 网关启动类
+ */
+@EnableDiscoveryClient
+@SpringBootApplication
+public class GateWayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GateWayApplication.class, args);
+    }
+}
+
